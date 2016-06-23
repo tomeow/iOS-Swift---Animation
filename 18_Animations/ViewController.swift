f2dedbe7509ad21457c91d04587806baf6236280
Tomomi <tomomi.tamura2486@gmail.com>
2016-06-23 14:55:48 -0700

    first commit

--- 18_Animations/ViewController.swift
+++ 18_Animations/ViewController.swift
@@ -9,17 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter = 1
+    
+    var timer = NSTimer()
+    
+    var isAnimating = true
+    
+    @IBOutlet var alienImage: UIImageView!
+    
+    @IBAction func updateImage(sender: AnyObject) {
+        
+        if isAnimating == true {
+            
+            timer.invalidate()
+            
+            isAnimating = false
+            
+        } else {
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            
+            isAnimating = true
+            
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+        
+        
     }
-
+    
+    func doAnimation() {
+        
+        if counter == 5 {
+            
+            counter = 1
+            
+        } else {
+            
+            counter++
+            
+        }
+        
+        alienImage.image = UIImage(named: "frame\(counter).png")
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+    /*
+    override func viewDidLayoutSubviews() {
+ 
+    alienImage.frame = CGRectMake(100, 20, 0, 0)
+ 
+    }
+ 
+    override func viewDidAppear(animated: Bool) {
+ 
+    UIView.animateWithDuration(1, animations: { () -> Void in
+ 
+    self.alienImage.frame = CGRectMake(100, 20, 100, 200)
+ 
+    })
+ 
+ 
+    }
+ 
+    */
+
+}
